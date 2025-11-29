47a0c3203e3c2de4e150194c074fb893dcff1397
progFilthi <gamerfilthi@gmail.com>
2025-11-29 23:59:17 +0200

    Added a default admin user on app start using commandLineRunner script

--- src/main/java/com/progfilthi/filthiblog/FilthiBlogApplication.java
+++ src/main/java/com/progfilthi/filthiblog/FilthiBlogApplication.java
@@ -1,9 +1,12 @@
 package com.progfilthi.filthiblog;
 
+import com.progfilthi.filthiblog.config.AdminProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AdminProperties.class)
 public class FilthiBlogApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/progfilthi/filthiblog/config/AdminProperties.java
+++ src/main/java/com/progfilthi/filthiblog/config/AdminProperties.java
@@ -1,9 +1,7 @@
 package com.progfilthi.filthiblog.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "app.admin")
 public record AdminProperties(
         String username,
